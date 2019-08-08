b00b02ea5c24cbe5d84cc8e64a880cfc5176ff70
Julian <Julian_cn@126.com>
2019-08-08 08:51:58 +0800

    晨讲_03

--- morning_speak_03.sh
+++ morning_speak_03.sh
@@ -9,3 +9,27 @@
 
 # 3. 如果给的目录存在，则下载文件；下载命令执行结束后，测试文件下载成功与否；
 #    如果成功，则返回0给调用脚本；否则，返回52给调用脚本。
+
+URL='http://pic33.nipic.com/20131007/13639685_123501617185_2.jpg'
+
+function get_file() {
+    read -p '输入存储目录：' path 
+
+    if [ ! -d "${path}" ]; then 
+       read -p "目录不存在，是否创建：（Y、N）" request 
+       [ ${request} == "Y" ] && mkdir ${path}; wget ${URL} -P ${path} || echo "退出脚本`exit 51`"
+    else 
+       wget ${URL} -P "${path}"
+       if [ $? -eq 0 ]; then 
+           echo "下载成功" 
+       else 
+           echo "下载失败"
+	   exit 52 
+       fi
+    fi 
+}
+
+get_file 
+
+
+
